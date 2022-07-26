693e817d8bb9681a424a2dc446c6282f2d419480
zengdawei <zengdaweiooo@gmail.com>
2022-07-26 11:39:40 +0800

    fix contract address bug

--- contract/event_handler.go
+++ contract/event_handler.go
@@ -2,15 +2,17 @@ package contract
 
 import (
 	"context"
+
 	"github.com/ZengDaWei/go-ethereum/address"
 	"github.com/ZengDaWei/go-ethereum/route"
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
 )
 
 func HandleEvent(ctx context.Context, log types.Log) error {
 	for _, contractAddress := range address.ContractAddresses {
-		if log.Address.String() != contractAddress {
+		if log.Address != common.HexToAddress(contractAddress) {
 			continue
 		}
 		for event, handleFunction := range route.EventHandleMap[contractAddress] {
